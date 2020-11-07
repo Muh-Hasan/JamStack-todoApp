4cd7946ee89ee2cccd76bd56f5392183d0070917
Muh-Hasan <muh.hasan.git@gmail.com>
2020-11-07 12:46:23 +0500

    functions done

--- functions/todo/todo.js
+++ functions/todo/todo.js
@@ -1,17 +1,78 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+const { ApolloServer, gql } = require("apollo-server-lambda")
+const faunadb = require("faunadb")
+q = faunadb.query
 
-module.exports = { handler }
+require("dotenv").config()
+
+const typDefs = gql`
+  type Query {
+    allTask: [Task]
+  }
+  type Mutation {
+    addTask(text: String): Task
+    delTask(id: ID!): Task
+  }
+  type Task {
+    id: ID!
+    text: String!
+  }
+`
+const client = new faunadb.Client({
+  secret: process.env.DB_SECRET,
+})
+
+const resolvers = {
+  Query: {
+    allTask: async () => {
+      try {
+        const result = await client.query(
+          q.Map(
+            q.Paginate(q.Documents(q.Collection("todos"))),
+            q.Lambda(x => q.Get(x))
+          )
+        )
+        const data = result.map(t => {
+          return {
+            id: t.ref.id,
+            text: t.data.text,
+          }
+        })
+        return data
+      } catch (error) {
+        console.log(error)
+        return error.toString()
+      }
+    },
+  },
+
+  Mutation: {
+    addTask: async (_, { text }) => {
+      try {
+        const result = await client.query(
+          q.Create(q.Collection("todo"), { data: { text: text } })
+        )
+        return result.data
+      } catch (error) {
+        return error.toString()
+      }
+    },
+    delTask: async (_, { id }) => {
+      try {
+        const newId = JSON.stringify(id)
+        console.log(newId)
+        const result = await client.query(
+          q.Delete(q.Ref(q.Collection("todos"), id))
+        )
+        return result.data
+      } catch (error) {
+        return error
+      }
+    },
+  },
+}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+exports.handler = server.createHandler()
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import React from "react"
+import React , { useState } from "react"
+import { useMutation , useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+
+const getTodos = gql`
+  {
+    allTask {
+      id
+      text
+    }
+  }
+`
+const addTodo = gql`
+  mutation addTask($text: String!){
+    addTask(text : $text)
+  }
+`
 
 export default function Home() {
+  const [todo , setTodo] = useState('')
   return( 
     <div>
-      <h1>hello world</h1>
+      <div>
+        <input type='text' placeholder='type todo' onChange={(e) => setTodo(e.currentTarget.value)} />
+        <button>+</button>
+      </div>
     </div>
   )
 }
